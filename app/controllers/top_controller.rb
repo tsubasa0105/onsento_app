265a4916f11563b27bbca096cd803fea448c04ba
tsubasaishikawa <t.ishikawa0105@gmail.com>
2019-01-19 15:56:42 +0900

    first commit

---
+++ app/controllers/top_controller.rb
@@ -0,0 +1,8 @@
+class TopController < ApplicationController
+  def index
+    @message = "こんにちは"
+  end
+
+  def about
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  root "top#index"
+
+  get "/about" => "top#about"
+end
